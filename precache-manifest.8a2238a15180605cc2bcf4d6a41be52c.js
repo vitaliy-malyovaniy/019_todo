561dad75e82dc36c3f544f3de248de46cdc7ba4d
VitaliyM <mva081972@gmail.com>
2019-02-10 17:04:42 +0200

    Updates

---
+++ precache-manifest.8a2238a15180605cc2bcf4d6a41be52c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ade9b30fdbd9878b850",
+    "url": "/vitaliy-malyovaniy/019_todo/static/js/runtime~main.2ade9b30.js"
+  },
+  {
+    "revision": "a4e09d5741032a96260e",
+    "url": "/vitaliy-malyovaniy/019_todo/static/js/main.a4e09d57.chunk.js"
+  },
+  {
+    "revision": "14396042d7b29abd1f9c",
+    "url": "/vitaliy-malyovaniy/019_todo/static/js/1.14396042.chunk.js"
+  },
+  {
+    "revision": "a4e09d5741032a96260e",
+    "url": "/vitaliy-malyovaniy/019_todo/static/css/main.395a8711.chunk.css"
+  },
+  {
+    "revision": "11260067d05cab49322f95e824bc7488",
+    "url": "/vitaliy-malyovaniy/019_todo/index.html"
+  }
+];
